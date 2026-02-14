d82721a8658a66342bfe3659c985c5434ead7bba
devendrasaim <mdevendrasai9@gmail.com>
2026-02-13 19:45:20 -0500

    Force disable all build checks

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,18 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
-}
+};
 
-export default nextConfig
+export default nextConfig;
